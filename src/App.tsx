b392205336db092b8906fd2e8a7c68a9f0506526
Mathilda <me@mathilda.dev>
2021-09-27 18:38:28 -0400

    Add eslint & typescript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { ReactElement } from 'react';
+import './App.css';
+
+const App = (): ReactElement => {
+  return (
+    <div className='App'>
+      Get hacking!
+    </div>
+  );
+};
+
+export default App;
